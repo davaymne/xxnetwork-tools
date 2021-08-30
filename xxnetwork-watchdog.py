1ed341e7ff39dfb9f4c09acfc704295255de8368
davaymne <davaymne.misc@gmail.com>
2021-08-30 01:18:49 +0100

    Update xxnetwork-watchdog.py

--- xxnetwork-watchdog.py
+++ xxnetwork-watchdog.py
@@ -5,7 +5,7 @@ import requests
 
 good_status = ['sleeping', 'idle', 'running', 'stopped']
 
-def telegram_bot_sendtext(bot_msg):
+def telegramBotSendText(bot_msg):
     bot_token = 'YOUR TOKEN'
     bot_chatID = 'YOU CHAT ID'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_msg
@@ -30,6 +30,6 @@ while True:
     p_status_list = getStatus()
     if len(p_status_list) > 0:
         for process in p_status_list:
-            telegram_bot_sendtext('Process: {}, status: {} - restarting now.'.format(process['name'], process['status']))
+            telegramBotSendText('Process: {}, status: {} - restarting now.'.format(process['name'], process['status']))
             restartProcess(["sudo", "/bin/systemctl", "restart", process['name']])
     time.sleep(300)
